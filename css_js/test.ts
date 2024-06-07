27cd1254512a0b5d3afa9ce8265ce141b368cc50
class5class <3060760656@qq.com>
2024-06-07 08:06:45 +0800

    test

---
+++ css_js/test.ts
@@ -0,0 +1,8 @@
+function some_func(arg: "a" | "b" | "c"): void {
+    if (arg === "a") { } else if (arg === "b") { } else if (arg === "c") { } else {
+        let some_var = arg;
+
+
+        some_var
+    }
+}
